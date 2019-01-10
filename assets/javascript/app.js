65f098683c252612d22db7e89801dad2b93a315f
Orlando Carnate <orlando.carnate@gmail.com>
2019-01-10 00:40:43 -0600

    Reduce app.js code from 445 lines to 376 lines. Tweak styles again.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,13 +10,18 @@ $(document).ready(function () {
     var maxtime = 15;
     var transitionTime = 7;
     var currentTrivia;
-    // create empty array for the shuffled indexes
-    var shuffledIndex = [];
-    // audio sound
-    var soundList = [
-            "magicspell1.mp3", "magicspell2.mp3", "magicspell3.mp3"
-        ];
-    var soundFX = document.createElement("audio");
+    var shuffledIndex = []; // create empty array for the shuffled indexes
+    var soundList = ["assets/sounds/wrong1.mp3", "assets/sounds/wrong2.mp3"];
+    var soundFX = document.createElement("audio"); // create audio element in the Document
+    var music = document.createElement("audio");
+    var endMusic =  document.createElement("audio");
+    music.setAttribute("src", "assets/sounds/fairytale1.mp3");
+    music.volume = 0.25;
+    music.loop = true;
+    endMusic.setAttribute("src", "assets/sounds/intro.mp3");
+    endMusic.volume = 1;
+    endMusic.loop = true;
+    soundFX.volume = 0.75;
 
     // Trivia data. First Property is the question. Second Property is a list of possible anwsers.
     var triviaData = [
@@ -24,7 +29,6 @@ $(document).ready(function () {
             question: "Which spell was invented by Severus Snape?",
             answers: ["Sectumsempra", "Riddikulus", "Steuleus", "Serpensortia"],
             answer: 0,
-            // pic: "https://gph.is/28UeI4D"
             pic: "https://media.giphy.com/media/R2nvBkAW7XIRy/200w_d.gif"
         },
         {
@@ -37,14 +41,12 @@ $(document).ready(function () {
             question: "What type of incantation is Wingardium Leviosa?",
             answers: ["Spell", "Curse", "Charm", "Jinx"],
             answer: 2,
-            // pic: "https://gph.is/16lTO9j"
-            pic: "https://media.giphy.com/media/KGHzRH9EGt5mw/200w_d.gif"
+            pic: "https://media.giphy.com/media/pI2paNxecnUNW/giphy.gif"
         },
         {
             question: "What does Ferula do?",
             answers: ["Turns the object into stone", "Creates a high-pitched sound", "Creates bandages", "Nothing, really"],
             answer: 2,
-            // pic: "https://gph.is/2eBjlIB"
             pic: "https://media.giphy.com/media/3ofT5YwE2opHvw6bQc/giphy-downsized.gif",
         },
         {
@@ -55,112 +57,62 @@ $(document).ready(function () {
         },
         {
             question: "What spell causes a blindfold over your eyes?",
-            answers: [
-                        "Caecusare", 
-                        "Opertis", 
-                        "Nonvidere", 
-                        "Obscuro"
-                    ],
-            answer: 3, // MUST BE A NUMBER FROM 0-3
-            pic: "https://media.giphy.com/media/10oAskWzsiEL8Q/giphy-downsized.gif"
+            answers: ["Caecusare", "Opertis", "Nonvidere", "Obscuro"],
+            answer: 3,
+            pic: "https://media.giphy.com/media/1BFGFPPuIRTUPZFy4d/giphy-downsized.gif"
         },
         {
             question: "What was Harry Potter's pet's name?",
-            answers: [
-                    "Jerry", 
-                    "Hedwig", 
-                    "Oliver", 
-                    "Hagrid"
-                    ],
-            answer: 1, // MUST BE A NUMBER FROM 0-3
+            answers: ["Jerry", "Hedwig", "Oliver", "Hagrid"],
+            answer: 1,
             pic: "https://media.giphy.com/media/CJ3PRGEzpTzVK/giphy-downsized.gif"
         },
         {
             question: "Which of these is NOT a Deathly Hallow?",
-            answers: [
-                    "Resurrection Stone", 
-                    "Cloak of Invisibility", 
-                    "Potion of Power", 
-                    "Elder Wand"
-                    ],
-            answer: 2, // MUST BE A NUMBER FROM 0-3
+            answers: ["Resurrection Stone", "Cloak of Invisibility", "Potion of Power", "Elder Wand"],
+            answer: 2,
             pic: "https://media.giphy.com/media/CK4BZhVG3689O/giphy-downsized.gif"
         },
         {
             question: "What is a Thestral's Classification?",
-            answers: [
-                "X", 
-                "XX", 
-                "XXX", 
-                "XXXX"
-                ],
-            answer: 3, // MUST BE A NUMBER FROM 0-3
+            answers: ["X", "XX", "XXX", "XXXX"],
+            answer: 3,
             pic: "https://media.giphy.com/media/f0OjjIH6I68GA/giphy-downsized.gif"
         },
         {
-            question: "Who is Hagrid?", // FIX
-            answers: [
-                "A Spider", 
-                "An Elephant", 
-                "A Giant", 
-                "An Executioner"
-                ],
-            answer: 2, // MUST BE A NUMBER FROM 0-3
+            question: "Who is Hagrid?",
+            answers: ["A Spider","An Elephant","A Giant","An Executioner"],
+            answer: 2,
             pic: "https://media.giphy.com/media/5zdcxaljX66mQ/giphy-downsized.gif"
         },
         {
             question: "Who killed Dumbledore?",
-            answers: [
-                "Voldemort", 
-                "Severus Snape", 
-                "Antonin Dolohov", 
-                "No one"
-                ],
-            answer: 1, // MUST BE A NUMBER FROM 0-3
+            answers: ["Voldemort", "Severus Snape", "Antonin Dolohov", "No one"],
+            answer: 1,
             pic: "https://media.giphy.com/media/OHNvxhuOdR1te/giphy-downsized.gif"
         },
         {
             question: "Who was Severus Snape in love with?",
-            answers: [
-                "Leta Lestrange", 
-                "Molly Weasley", 
-                "Lily Potter", 
-                "No one"
-                ],
-            answer: 2, // MUST BE A NUMBER FROM 0-3
+            answers: ["Leta Lestrange", "Molly Weasley", "Lily Potter", "No one" ],
+            answer: 2,
             pic: "https://media.giphy.com/media/qEabWrZXscSTm/giphy.gif"
         },
         {
             question: "What is Felix Felicis?",
-            answers: [
-                "Potion of Love", 
-                "Potion of Death", 
-                "Potion of Resistance", 
-                "Potion of Luck"
-                ],
-            answer: 3, // MUST BE A NUMBER FROM 0-3
+            answers: ["Potion of Love", "Potion of Death", "Potion of Resistance", "Potion of Luck"],
+            answer: 3,
             pic: "https://media.giphy.com/media/WLXYBlFuBiuQM/giphy.gif"
         },
     	{
             question: "What does the spell Lumos do?",
-            answers: [
-                "Creates Light", 
-                "Creates Fire", 
-                "Creates Ice", 
-                "Creates Darkness"
-                ],
-            answer: 0, // MUST BE A NUMBER FROM 0-3
+            answers: ["Creates Light", "Creates Fire", "Creates Ice", "Creates Darkness"],
+            answer: 0,
             pic: "https://media.giphy.com/media/Q8WGAVxruhAIg/giphy-downsized.gif"
         },
     	{
             question: "What does the curse Avada Kedavra do?",
-            answers: [
-                "A Curse of Fire", 
-                "A Curse Of Light", 
-                "A Killing Curse", 
-                "A Curse Of Slime"
-                ],
-            answer: 2, // MUST BE A NUMBER FROM 0-3
+            answers: ["A Curse of Fire",  "A Curse Of Light", "A Killing Curse", "A Curse Of Slime"],
+            answer: 2,
             pic: "https://media.giphy.com/media/rMYBNQ6LfVV5u/giphy-downsized.gif"
         },
 
@@ -171,6 +123,31 @@ $(document).ready(function () {
     $("#endtext").hide();
     $("#time-remaining").hide();
 
+    // event listeners
+    $("#start").click(function() {
+        music.play()
+        triviaGame.askQuestion();
+    });
+
+    $("#restart").click(function() {
+        triviaGame.randomFX();
+        triviaGame.restartGame();
+    });
+
+    $("#answers").on('click', '.answer', function() {
+        clearInterval(timer);
+        triviaGame.checkAnswer($(this).attr("value"));
+    });
+
+    $("#answers").on('mouseover', '.answer', function() {
+        // alert("mouseover");
+        $(this).animate({"font-size": "26pt"}, "fast");
+    });
+    $("#answers").on('mouseleave', '.answer', function() {
+        // alert("mouseover");
+        $(this).animate({"font-size": "20pt"}, "fast");
+    });
+
     // Trivia Game Object
     var triviaGame = {
 
@@ -178,36 +155,23 @@ $(document).ready(function () {
         shuffle: function () {
             // reset shuffledIndex array
             shuffledIndex = [];
-            console.log("Current:", shuffledIndex);
-            // create an array with item numbers based on length of trivia data
             for (var i=0; i < triviaData.length; i++) {
                 shuffledIndex.push(i);
             }
-            console.log("Shuffled: ",shuffledIndex);
-    
-    
-            // gets the length 
             var randomIndex;
             var tempItem;
             for (i = shuffledIndex.length - 1; i > 0; i--) {
                 randomIndex = Math.floor(Math.random() * (i+1));
-                // copy current item in index i
-                tempItem = shuffledIndex[i];
-                // copy random item to current index i in array
-                shuffledIndex[i] = shuffledIndex[randomIndex];
-                // copy temp item to random index
-                shuffledIndex[randomIndex] = tempItem;
-                console.log(shuffledIndex);
-    
+                tempItem = shuffledIndex[i]; // copy current item in index i
+                shuffledIndex[i] = shuffledIndex[randomIndex]; // copy random item to current index i in array
+                shuffledIndex[randomIndex] = tempItem; // copy temp item to random index
             }
-            console.log("Result:",shuffledIndex);
+            console.log("Shuffled Questions Array: ",shuffledIndex);
         },
 
         // random sound fx
         randomFX: function () {
-            randomIndex = Math.floor(Math.random() * soundList.length);
-            var fxSource = "assets/sounds/" + soundList[randomIndex];
-            console.log("SoundFX path: ", fxSource);
+            var fxSource = soundList[Math.floor(Math.random() * soundList.length)];
             soundFX.setAttribute("src", fxSource)
             soundFX.play();
         },
@@ -215,9 +179,6 @@ $(document).ready(function () {
         // displays the question and possible answers from the trivia array
         displayQuestion: function () {
             currentTrivia = triviaData[shuffledIndex[currentQuestionIndex]];
-            console.log("Current Q number: ", currentQuestionIndex);
-            console.log("Current Trivia data:",currentTrivia);
-            console.log("Current Q", currentTrivia.question);
             console.log(currentTrivia);
 
             // show the trivia question
@@ -225,10 +186,9 @@ $(document).ready(function () {
     
             // use for loop to display possible answers
             for (var i=0; i < currentTrivia.answers.length; i++) {
-                console.log(currentTrivia.answers[i]);
                 answerID = i;
-                var answerListItem = $("<div/>", {"class": "answer", "value": i, text: currentTrivia.answers[i]});
-                $("#answers").append(answerListItem);
+                var $answerListItem = $("<div/>", {"class": "answer", "value": i, text: currentTrivia.answers[i]});
+                $("#answers").append($answerListItem);
             }
         },
 
@@ -238,7 +198,7 @@ $(document).ready(function () {
             $("#picAnswer").append(answerPic);
         },
 
-        // determines of there more questions or go to end of game.
+        // determines if there more questions or go to end of game.
         askQuestion: function () {
             // clear any generated elements with empty() method
             $("#question").empty();
@@ -262,8 +222,6 @@ $(document).ready(function () {
                 // goto endGame
                 this.endGame();
             }
-    
-    
         },
 
         // checks the clicked answer if it's correct
@@ -273,12 +231,10 @@ $(document).ready(function () {
                 // tell player is correct and add to CORRECT score.
                 correct++;
                 $("#question").text('Correct!');
-                $("#answers").html('You Picked <h5><strong>"' + currentTrivia.answers[currentTrivia.answer] + '"</strong></h4>');
+                $("#answers").html('You Picked <h4><spa class="correctanswer">"' + currentTrivia.answers[currentTrivia.answer] + '"</span></h4>');
     
                 // display pic
                 this.displayPic(currentTrivia.pic);
-                // var answerPic = $("<img/>", {"class": "pic", "src": currentTrivia.pic});
-                // $("#picAnswer").append(answerPic);
 
                 // play a Correct Sound
                 soundFX.setAttribute("src", "assets/sounds/correct.mp3");
@@ -288,15 +244,12 @@ $(document).ready(function () {
             else {
                 // play random sound
                 this.randomFX();
+
                 // tell player is wrong and add to Wrong score.
+                $("#question").text("Wrong Answer."); 
+                $("#answers").html('The correct answer is <h4><span class="correctanswer">"' + currentTrivia.answers[currentTrivia.answer] + '"</span></h4>');
                 wrong++;
-                $("#question").text("Wrong Answer.");
-                $("#answers").html('The correct answer is <h5><strong>"' + currentTrivia.answers[currentTrivia.answer] + '"</strong></h5>');
-    
-                // display pic
-                this.displayPic(currentTrivia.pic);
-                // var answerPic = $("<img/>", {"class": "pic", "src": currentTrivia.pic});
-                // $("#picAnswer").append(answerPic);
+                this.displayPic(currentTrivia.pic); // display pic
             }
     
                 // see if this is the last question
@@ -307,6 +260,9 @@ $(document).ready(function () {
         startTimer: function () {
             // assign max time to time
             var time = maxtime;
+
+            // display hourglass
+            // triviaGame.displayPic("https://media.giphy.com/media/117AhFsciLHEJy/giphy-downsized.gif");
 
             // display time remaining
             $("#time-remaining").show();
@@ -330,19 +286,28 @@ $(document).ready(function () {
                     // show correct answer
                     triviaGame.randomFX();
                     $("#question").text("Times Up!");
-                    $("#answers").html('The correct answer is <h5><strong>"' + currentTrivia.answers[currentTrivia.answer] + '"<strong></h4>')
+                    $("#answers").html('The correct answer is <h4><span class="correctanswer">"' + currentTrivia.answers[currentTrivia.answer] + '"</span></h4>')
     
                     // display pic
                     triviaGame.displayPic(currentTrivia.pic);
-                    // var answerPic = $("<img/>", {"class": "pic", "src": currentTrivia.pic});
-                    // $("#picAnswer").append(answerPic);
     
                     // check if the next question is the last
                     triviaGame.nextTimerHeadiing();
                 }
-    
             }, 1000);
+        },
+
+        nextTimerHeadiing: function () {
+            // if there are still questions left, tell player the next question will be in 5 seconds
+            if (currentQuestionIndex < maxQuestions) {
+                $("#transition-text").text("Next Question in ");
     
+            // if it was the last question then tell player score is next in 5 seconds
+            } else {
+                $("#transition-text").text("Let's see your score in ");
+            }
+            //  run the transition timer
+            this.nextTimer();
         },
 
         // a transition timer between questions
@@ -370,20 +335,6 @@ $(document).ready(function () {
             }, 1000)
         },
 
-        nextTimerHeadiing: function () {
-            // if there are still questions left, tell player the next question will be in 5 seconds
-            if (currentQuestionIndex < maxQuestions) {
-                $("#transition-text").text("Next Question in ");
-    
-            // if it was the last question then tell player score is next in 5 seconds
-            } else {
-                $("#transition-text").text("Let's see your score in ");
-            }
-            
-            //  run the transition timer
-            this.nextTimer();
-        },
-
         restartGame: function () {
             // clear any generated elements with empty() method
             correct = 0;
@@ -400,46 +351,26 @@ $(document).ready(function () {
             
             // display the trivia question
             this.askQuestion();
-    
         },
 
         endGame: function () {
+            music.pause();
+            endMusic.play();
             // clear or hide all elements
             $("#time-remaining").hide();
             $("#transition").hide();
     
             // display final tally
             $("#endtext").show();
-    
             $("#correct").text(correct);
             $("#wrong").text(wrong);
             $("#unanswered").text(unanswered);
     
             // display restart button
             $("#restart").show();
-    
         },
     }
 
     // shuffle the trivia
     triviaGame.shuffle();
-
-    // event listeners
-    $("#start").click(function() {
-        triviaGame.randomFX();
-        triviaGame.askQuestion();
-    });
-
-    $("#restart").click(function() {
-        triviaGame.randomFX();
-        triviaGame.restartGame();
-    });
-
-    $("#answers").on('click', '.answer', function() {
-        // alert($(this).attr("value"));
-        clearInterval(timer);
-        triviaGame.checkAnswer($(this).attr("value"));
-    });
-
-
 });
